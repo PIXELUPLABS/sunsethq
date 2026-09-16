5ba0339bed346d86e316449dc23b171d58e3f685
pixeluplabs <design@pixelup.in>
2026-09-16 17:04:11 +0530

    changes for the hero section height in home page

--- modules/landing/components/hero-section.tsx
+++ modules/landing/components/hero-section.tsx
@@ -53,7 +53,7 @@ export function HeroSection() {
 
   return (
     <div ref={pinRef} className="relative lg:h-[475vh]">
-      <section className="relative overflow-hidden border-b border-dashed border-black/8 bg-[#fcfcfc] lg:sticky lg:top-0 lg:flex lg:min-h-[80vh] lg:flex-col min-[1800px]:min-h-[100vh]!">
+      <section className="relative overflow-hidden border-b border-dashed border-black/8 bg-[#fcfcfc] lg:sticky lg:top-0 lg:flex lg:min-h-[100vh] lg:flex-col">
         <Image
           src="/images/grain-light-texture.svg"
           alt=""
